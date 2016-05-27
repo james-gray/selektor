9ebaff404a648fafd022bcc2be726e41b9ab1af6
James Gray <james.gray@mediacore.com>
2016-05-26 20:08:01 -0700

    Initial Commit

---
+++ Selektor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Selektor
+//
+//  Created by James Gray on 2016-05-26.
+//  Copyright © 2016 James Gray. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
