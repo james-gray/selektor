b95152ce5fe2605a8c45c8c459fed1d78098f9f1
James Gray <james.gray@mediacore.com>
2016-05-29 21:02:18 -0700

    Add a DataController class for interfacing with the persistence tier

---
+++ Selektor/DataController.swift
@@ -0,0 +1,89 @@
+//
+//  DataController.swift
+//  Selektor
+//
+//  Created by James Gray on 2016-05-28.
+//  Copyright © 2016 James Gray. All rights reserved.
+//
+
+import CoreData
+
+// Add any new managed object model classes here so the DataController can maintain
+// a consistent mapping of entities to their model classes
+
+class DataController: NSObject {
+  // Based on sample code provided by Apple at:
+  // developer.apple.com/library/mac/documentation/Cocoa/Conceptual/CoreData/
+  //     InitializingtheCoreDataStack.html#//apple_ref/doc/uid/TP40001075-CH4-SW1
+  //
+  // Copyright © 2016 Apple Inc. All rights reserved.
+  var managedObjectContext: NSManagedObjectContext
+  
+  override init() {
+    guard let modelURL = NSBundle.mainBundle().URLForResource(
+        "DataModel",
+        withExtension: "momd"
+    ) else {
+      fatalError("Error loading model from bundle.")
+    }
+    
+    // The managed object model for the application. It is a fatal error for the
+    // application not to be able to find and load its model.
+    guard let mom = NSManagedObjectModel(contentsOfURL: modelURL) else {
+      fatalError("Error initializing mom from: \(modelURL)")
+    }
+    
+    let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
+
+    managedObjectContext = NSManagedObjectContext(
+        concurrencyType: .MainQueueConcurrencyType
+    )
+    managedObjectContext.persistentStoreCoordinator = psc
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+      let urls = NSFileManager.defaultManager().URLsForDirectory(
+          .DocumentDirectory,
+          inDomains: .UserDomainMask
+      )
+
+      let docURL = urls[urls.endIndex-1]
+
+      // The directory the application uses to store the Core Data store file.
+      // This code uses a file named "DataModel.sqlite" in the application's
+      // documents directory.
+      let storeURL = docURL.URLByAppendingPathComponent("DataModel.sqlite")
+      do {
+        try psc.addPersistentStoreWithType(NSSQLiteStoreType,
+                                           configuration: nil,
+                                           URL: storeURL,
+                                           options: nil)
+      } catch {
+        fatalError("Error migrating store: \(error)")
+      }
+    }
+  }
+
+  func createNewEntity(entityName: String) -> NSManagedObject {
+    let object = NSEntityDescription.insertNewObjectForEntityForName(entityName,
+        inManagedObjectContext: managedObjectContext)
+    return object
+  }
+
+  func fetchEntities(entityName: String, predicate: String?) -> [NSManagedObject] {
+
+    let moc = managedObjectContext
+    let fetchRequest = NSFetchRequest(entityName: entityName)
+
+    if let predicate = predicate {
+      // Filter the results
+      fetchRequest.predicate = NSPredicate(format: predicate)
+    }
+
+    do {
+      let fetchedObjects = try moc.executeFetchRequest(fetchRequest) as! [NSManagedObject]
+      return fetchedObjects
+    } catch {
+      fatalError("Failed to fetch \(entityName)s: \(error)")
+    }
+  }
+}
