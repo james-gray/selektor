474549fb36758ab2ac7d5e13bce22f11f19d9876
James Gray <james.gray@mediacore.com>
2016-06-16 22:40:11 -0700

    Update docs

--- Selektor/Models/SelektorObject.swift
+++ Selektor/Models/SelektorObject.swift
@@ -10,12 +10,16 @@ import CoreData
 
 // Subclass of NSManagedObject that entity classes should subclass for the purpose
 // of exposing their entity names to the DataController via the `getEntityName` method.
+// NOTE: This could also be accomplished with an NSManagedObject extension, however
+// I've opted to simply subclass NSManagedObject in case I need other common
+// functionality that only makes sense within the context of this app.
 class SelektorObject: NSManagedObject {
 
   // XXX: Hack due to Swift's lack of support for class vars as of yet.
   // A `class func` is effectively equivalent to a `static func`, but can be
-  // overridden by subclasses. `static var`s cannot be overridden, so we must
-  // use a getter instead.
+  // overridden by subclasses (unlike static funcs.)
+  // Similarly, `static var`s cannot be overridden (and `class vars` don't exist)
+  // so we must use a getter method instead.
   class func getEntityName() -> String {
     print("Subclasses should override abstract method `getEntityName`!")
     abort()
