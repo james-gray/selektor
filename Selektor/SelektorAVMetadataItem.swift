930e5e52e8b7e2284579353c219093c75a9357e4
James Gray <james.gray@mediacore.com>
2016-06-20 23:45:36 -0700

    Add AVMetadataItem extension to allow access to sane string keys on meta items

---
+++ Selektor/SelektorAVMetadataItem.swift
@@ -0,0 +1,65 @@
+//
+//  SelektorAVMetadataItem.swift
+//  Selektor
+//
+//  Created by James Gray on 2016-06-20.
+//  Copyright © 2016 James Gray. All rights reserved.
+//
+
+import AVFoundation
+import Foundation
+
+extension AVMetadataItem {
+  /**
+   * Ugly mess of type conversion, pointers and C strings to generate a string
+   * representation of the metadata item's key. Whyyy is this not a part of the
+   * AVFoundation API already, Apple?!
+   *
+   * Based on code by Bob McCune in "Learning AV Foundation: A Hands-on Guide to
+   * Mastering the AV Foundation Framework."
+   * See https://itunes.apple.com/us/book/learning-av-foundation/id934379880?mt=11
+   */
+  var keyString: String {
+    let copyrightChar: Int8 = 97
+    let diaeresisChar: Int8 = -87
+
+    if let key = self.key as? String {
+      return key
+    }
+
+    if let key = self.key as? Int {
+      var keyValue = UInt32(truncatingBitPattern: key)
+      var length = sizeof(UInt32)
+
+      // Most keys are 4 characters, however ID3v2.2 keys are 3 characters
+      // long - adjust the length as necessary
+      if (keyValue >> 24) == 0 { length -= 1 }
+      if (keyValue >> 16) == 0 { length -= 1 }
+      if (keyValue >> 8) == 0 { length -= 1 }
+      if (keyValue >> 0) == 0 { length -= 1 }
+
+      var address: UnsafeMutablePointer<UInt32> = withUnsafeMutablePointer(&keyValue, { $0 })
+      address += (sizeof(UInt32) - length)
+
+      // Keys are stored in big-endian format so we must swap
+      keyValue = CFSwapInt32BigToHost(keyValue)
+
+      let cstring = UnsafeMutablePointer<CChar>.alloc(length)
+      strncpy(cstring, UnsafePointer<CChar>(address), length)
+
+      // <Gross Hack> to null terminate C string. There has to be another way!
+      cstring[length] = CChar(Array("\0".utf8)[0])
+      // </Gross Hack>
+
+      if [copyrightChar, diaeresisChar].contains(cstring[0]) {
+        // Replace "©" with @
+        cstring[0] = CChar(Array("@".utf8)[0])
+      }
+
+      return NSString(UTF8String: cstring) as! String
+
+    } else {
+      return "<<unknown>>"
+    }
+  }
+}
