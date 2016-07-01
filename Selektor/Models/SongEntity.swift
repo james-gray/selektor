5da37de4f8f25f4856631f4cc420b6478fbf3389
James Gray <james.gray@mediacore.com>
2016-07-01 11:51:20 -0700

    Simplify TimbreVector to handle summarization type at a higher level

--- Selektor/Models/SongEntity.swift
+++ Selektor/Models/SongEntity.swift
@@ -32,7 +32,53 @@ class SongEntity: SelektorObject {
   @NSManaged dynamic var genre: String?
   @NSManaged dynamic var key: String?
   @NSManaged dynamic var label: String?
-  @NSManaged dynamic var timbre: TimbreVectorEntity?
+  @NSManaged dynamic var timbres: NSSet?
+
+  // MARK: Convenience getters for obtaining timbre vectors of different
+  // summarization types
+  dynamic var mammTimbre: TimbreVectorEntity? {
+    get { return self.getTimbreForSummaryType(SummaryType.MeanAccMeanMem) }
+    set { self.setTimbreForSummaryType(newValue!, summaryType: SummaryType.MeanAccMeanMem) }
+  }
+
+  dynamic var masmTimbre: TimbreVectorEntity? {
+    get { return self.getTimbreForSummaryType(SummaryType.MeanAccStdMem) }
+    set { self.setTimbreForSummaryType(newValue!, summaryType: SummaryType.MeanAccStdMem) }
+  }
+
+  dynamic var sammTimbre: TimbreVectorEntity? {
+    get { return self.getTimbreForSummaryType(SummaryType.StdAccMeanMem) }
+    set { self.setTimbreForSummaryType(newValue!, summaryType: SummaryType.StdAccMeanMem) }
+  }
+
+  dynamic var sasmTimbre: TimbreVectorEntity? {
+    get { return self.getTimbreForSummaryType(SummaryType.StdAccStdMem) }
+    set { self.setTimbreForSummaryType(newValue!, summaryType: SummaryType.StdAccStdMem) }
+  }
+
+  func getTimbreForSummaryType(summaryType: SummaryType) -> TimbreVectorEntity? {
+    return timbres!.filter({
+      ($0 as! TimbreVectorEntity).summaryType == summaryType.rawValue
+    }).first as? TimbreVectorEntity
+  }
+
+  func setTimbreForSummaryType(newVector: TimbreVectorEntity, summaryType: SummaryType) {
+    let timbresSet = self.mutableSetValueForKey("timbres")
+    // Remove old timbre vector if necessary
+    self.removeTimbreForSummaryType(timbresSet, summaryType: summaryType)
+
+    // Set the summary type for the new vector and add it to the timbres set
+    newVector.summaryType = summaryType.rawValue
+    timbresSet.addObject(newVector)
+  }
+
+  func removeTimbreForSummaryType(timbresSet: NSMutableSet?, summaryType: SummaryType) {
+    let timbresSet = timbresSet ?? self.mutableSetValueForKey("timbres")
+    if let oldVector = self.getTimbreForSummaryType(summaryType) {
+      // Remove the old vector for the summary type
+      timbresSet.removeObject(oldVector)
+    }
+  }
 
   override class func getEntityName() -> String {
     return "Song"
@@ -41,7 +87,7 @@ class SongEntity: SelektorObject {
   override func awakeFromInsert() {
     super.awakeFromInsert()
 
-    // Set dateAdded to the current date on object creation
+    // Set default values for NSManaged properties
     dateAdded = NSDate()
     analyzed = AnalysisState.ToDo.rawValue
   }
--- Selektor/Models/TimbreVectorEntity.swift
+++ Selektor/Models/TimbreVectorEntity.swift
@@ -9,77 +9,30 @@
 import Foundation
 import CoreData
 
+enum SummaryType: Int {
+  case MeanAccMeanMem = 0
+  case MeanAccStdMem
+  case StdAccMeanMem
+  case StdAccStdMem
+}
 
 class TimbreVectorEntity: SelektorObject {
 
   // MARK: Properties
-  @NSManaged var mammCentroid: NSNumber?
-  @NSManaged var masmCentroid: NSNumber?
-  @NSManaged var sammCentroid: NSNumber?
-  @NSManaged var sasmCentroid: NSNumber?
-  @NSManaged var mammFlux: NSNumber?
-  @NSManaged var masmFlux: NSNumber?
-  @NSManaged var sammFlux: NSNumber?
-  @NSManaged var sasmFlux: NSNumber?
-  @NSManaged var mammRolloff: NSNumber?
-  @NSManaged var masmRolloff: NSNumber?
-  @NSManaged var sammRolloff: NSNumber?
-  @NSManaged var sasmRolloff: NSNumber?
-  @NSManaged var mammMFCCString: String?
-  @NSManaged var masmMFCCString: String?
-  @NSManaged var sammMFCCString: String?
-  @NSManaged var sasmMFCCString: String?
+  @NSManaged var summaryType: NSNumber?
+  @NSManaged var centroid: NSNumber?
+  @NSManaged var flux: NSNumber?
+  @NSManaged var rolloff: NSNumber?
+  @NSManaged var mfccString: String?
   @NSManaged var song: SongEntity?
 
   // MARK: Public getters/setters for managed MFCC properties
-  var mammMFCC: [Double] {
+  var mfcc: [Double] {
     get {
-      return self.doubleArrayFromCsvString(self.mammMFCCString) ?? []
+      return self.mfccString?.componentsSeparatedByString(",").map { Double($0)! } ?? []
     }
     set {
-      self.mammMFCCString = self.csvStringFromDoubleArray(newValue)
+      self.mfccString = newValue.map({ String($0) }).joinWithSeparator(",")
     }
-  }
-
-  var mammsMFCC: [Double] {
-    get {
-      return self.doubleArrayFromCsvString(self.masmMFCCString) ?? []
-    }
-    set {
-      self.masmMFCCString = self.csvStringFromDoubleArray(newValue)
-    }
-  }
-
-  var sammMFCC: [Double] {
-    get {
-      return self.doubleArrayFromCsvString(self.sammMFCCString) ?? []
-    }
-    set {
-      self.sammMFCCString = self.csvStringFromDoubleArray(newValue)
-    }
-  }
-
-  var sasmMFCC: [Double] {
-    get {
-      return self.doubleArrayFromCsvString(self.sasmMFCCString) ?? []
-    }
-    set {
-      self.sasmMFCCString = self.csvStringFromDoubleArray(newValue)
-    }
-  }
-
-  /*
-      Serialize a double array into a string of doubles separated by commas for
-      storage in the database.
-   */
-  func csvStringFromDoubleArray(doubles: [Double]) -> String {
-    return doubles.map({ String($0) }).joinWithSeparator(",")
-  }
-
-  /*
-      Parse a CSV string of doubles from the database into an array of doubles.
-   */
-  func doubleArrayFromCsvString(csv: String?) -> [Double] {
-    return csv?.componentsSeparatedByString(",").map { Double($0)! } ?? []
   }
 }
