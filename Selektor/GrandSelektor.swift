e5b194089e6e558628cbcccb33ceaae7cb3ee68a
James Gray <james.gray@mediacore.com>
2016-07-20 12:59:22 -0700

    Implement rankedGenre and medianGenre algorithms and further dedupe code

--- Selektor/GrandSelektor.swift
+++ Selektor/GrandSelektor.swift
@@ -41,8 +41,8 @@ class GrandSelektor: NSObject {
       "medianBPM": self.selectTrackMedianBPM,
       "rankedLoudness": self.selectTrackRankedLoudness,
       "medianLoudness": self.selectTrackMedianLoudness,
-      //"rankedGenre": self.selectTrackRankedGenre,
-      //"medianGenre": self.selectTrackMedianGenre,
+      "rankedGenre": self.selectTrackRankedGenre,
+      "medianGenre": self.selectTrackMedianGenre,
       //"rankedKey": self.selectTrackRankedKey,
       //"medianKey": self.selectTrackMedianKey,
     ]
@@ -55,6 +55,11 @@ class GrandSelektor: NSObject {
 
       If no other tracks within the range of ± 3 BPM are available, increment
       the considered BPM range by 3 repeatedly until more tracks are found.
+
+      - parameter currentTrack: The track to compare tracks against.
+      - parameter tracks: The set of tracks to filter from.
+
+      - returns: A subset of tracks with similar BPM.
   */
   func findTracksWithSimilarBPM(toTrack currentTrack: TrackEntity, inSet tracks: [TrackEntity]) -> [TrackEntity] {
     var bpmOffset = 0
@@ -75,6 +80,27 @@ class GrandSelektor: NSObject {
   }
 
   /**
+      Given the current track, return a subset of tracks with the same genre as
+      the current track. If no other tracks with the same genre are available,
+      simply return all tracks.
+
+      - parameter currentTrack: The track to compare tracks against.
+      - parameter tracks: The set of tracks to filter from.
+
+      - returns: A subset of tracks with the same genre, or the original set if no
+            other tracks with the same genre are found.
+  */
+  func findTracksWithSameGenre(asTrack currentTrack: TrackEntity, inSet tracks: [TrackEntity]) -> [TrackEntity] {
+    guard let genre = currentTrack.genre else {
+      // Don't bother comparing this track if it doesn't have a genre
+      return tracks
+    }
+
+    let tracksSubset = tracks.filter { $0.genre == genre }
+    return tracksSubset.count > 0 ? tracksSubset : tracks
+  }
+
+  /**
       Selects a track within ± 3 BPM (if possible) with the shortest distance
       to the current track based on the distance function `distanceFunc`.
 
@@ -87,13 +113,11 @@ class GrandSelektor: NSObject {
   */
   func rankedSelection(withTrack currentTrack: TrackEntity, fromSet tracks: [TrackEntity],
       usingDistanceFunction distanceFunc: (TrackEntity, TrackEntity) -> Double) -> TrackEntity {
-    let trackSubset = findTracksWithSimilarBPM(toTrack: currentTrack, inSet: tracks)
-
     var minDistance = DBL_MAX
     var distance = 0.0
     var selectedTrack: TrackEntity? = nil
 
-    for otherTrack in trackSubset {
+    for otherTrack in tracks {
       distance = distanceFunc(currentTrack, otherTrack)
       if distance < minDistance {
         minDistance = distance
@@ -117,10 +141,8 @@ class GrandSelektor: NSObject {
    */
   func medianSelection(withTrack currentTrack: TrackEntity, fromSet tracks: [TrackEntity],
       usingDistanceFunction distanceFunc: (TrackEntity, TrackEntity) -> Double) -> TrackEntity {
-    let trackSubset = findTracksWithSimilarBPM(toTrack: currentTrack, inSet: tracks)
-
     // Compute the median distance
-    let distances = trackSubset.map { distanceFunc(currentTrack, $0) }
+    let distances = tracks.map { distanceFunc(currentTrack, $0) }
     let medianDistance = distances.sort()[distances.count / 2]
 
     // Compare the distances of each track from the current track to the median distance
@@ -205,11 +227,13 @@ class GrandSelektor: NSObject {
       - returns: The recommended track as deduced by the algorithm.
   */
   func selectTrackRankedBPM(currentTrack: TrackEntity, tracks: [TrackEntity]) -> TrackEntity {
+    let trackSubset = findTracksWithSimilarBPM(toTrack: currentTrack, inSet: tracks)
+
     let distanceFunc = { (currentTrack: TrackEntity, otherTrack: TrackEntity) in
       return currentTrack.compareTimbreWith(otherTrack: otherTrack)
     }
 
-    return rankedSelection(withTrack: currentTrack, fromSet: tracks,
+    return rankedSelection(withTrack: currentTrack, fromSet: trackSubset,
         usingDistanceFunction: distanceFunc)
   }
 
@@ -223,11 +247,13 @@ class GrandSelektor: NSObject {
       - returns: The recommended track as deduced by the algorithm.
   */
   func selectTrackMedianBPM(currentTrack: TrackEntity, tracks: [TrackEntity]) -> TrackEntity {
+    let trackSubset = findTracksWithSimilarBPM(toTrack: currentTrack, inSet: tracks)
+
     let distanceFunc = { (currentTrack: TrackEntity, otherTrack: TrackEntity) in
       return currentTrack.compareTimbreWith(otherTrack: otherTrack)
     }
 
-    return medianSelection(withTrack: currentTrack, fromSet: tracks,
+    return medianSelection(withTrack: currentTrack, fromSet: trackSubset,
         usingDistanceFunction: distanceFunc)
   }
 
@@ -245,12 +271,14 @@ class GrandSelektor: NSObject {
       - returns: The recommended track as deduced by the algorithm.
   */
   func selectTrackRankedLoudness(currentTrack: TrackEntity, tracks: [TrackEntity]) -> TrackEntity {
+    let trackSubset = findTracksWithSimilarBPM(toTrack: currentTrack, inSet: tracks)
+
     let distanceFunc = { (currentTrack: TrackEntity, otherTrack: TrackEntity) in
       return currentTrack.compareTimbreWith(otherTrack: otherTrack)
         + currentTrack.compareLoudnessWith(otherTrack: otherTrack)
     }
 
-    return rankedSelection(withTrack: currentTrack, fromSet: tracks,
+    return rankedSelection(withTrack: currentTrack, fromSet: trackSubset,
         usingDistanceFunction: distanceFunc)
   }
 
@@ -265,12 +293,60 @@ class GrandSelektor: NSObject {
       - returns: The recommended track as deduced by the algorithm.
   */
   func selectTrackMedianLoudness(currentTrack: TrackEntity, tracks: [TrackEntity]) -> TrackEntity {
+    let trackSubset = findTracksWithSimilarBPM(toTrack: currentTrack, inSet: tracks)
+
     let distanceFunc = { (currentTrack: TrackEntity, otherTrack: TrackEntity) in
       return currentTrack.compareTimbreWith(otherTrack: otherTrack)
         + currentTrack.compareLoudnessWith(otherTrack: otherTrack)
     }
 
-    return medianSelection(withTrack: currentTrack, fromSet: tracks,
+    return medianSelection(withTrack: currentTrack, fromSet: trackSubset,
         usingDistanceFunction: distanceFunc)
+  }
+
+  /**
+      Selects a track that is in the same genre as the current track (if possible),
+      within ± 3 BPM (if possible) of the current track's tempo, abd with the
+      most similar timbre and loudness to the current track.
+
+      - parameter currentTrack: The track that is currently playing.
+      - parameter tracks: An array of `TrackEntity`s to choose from.
+
+      - returns: The recommended track as deduced by the algorithm.
+  */
+  func selectTrackRankedGenre(currentTrack: TrackEntity, tracks: [TrackEntity]) -> TrackEntity {
+    var trackSubset = findTracksWithSameGenre(asTrack: currentTrack, inSet: tracks)
+    trackSubset = findTracksWithSimilarBPM(toTrack: currentTrack, inSet: trackSubset)
+
+    let distanceFunc = { (currentTrack: TrackEntity, otherTrack: TrackEntity) in
+      return currentTrack.compareTimbreWith(otherTrack: otherTrack)
+        + currentTrack.compareLoudnessWith(otherTrack: otherTrack)
+    }
+
+    return rankedSelection(withTrack: currentTrack, fromSet: trackSubset,
+        usingDistanceFunction: distanceFunc)
+  }
+
+  /**
+      Selects a track within ± 3 BPM (if possible) whose combined timbre and
+      loudness distance is closest to the median combined distance of all tracks
+      to the current track.
+
+      - parameter currentTrack: The track that is currently playing.
+      - parameter tracks: An array of `TrackEntity`s to choose from.
+
+      - returns: The recommended track as deduced by the algorithm.
+   */
+  func selectTrackMedianGenre(currentTrack: TrackEntity, tracks: [TrackEntity]) -> TrackEntity {
+    var trackSubset = findTracksWithSameGenre(asTrack: currentTrack, inSet: tracks)
+    trackSubset = findTracksWithSimilarBPM(toTrack: currentTrack, inSet: trackSubset)
+
+    let distanceFunc = { (currentTrack: TrackEntity, otherTrack: TrackEntity) in
+      return currentTrack.compareTimbreWith(otherTrack: otherTrack)
+        + currentTrack.compareLoudnessWith(otherTrack: otherTrack)
+    }
+
+    return medianSelection(withTrack: currentTrack, fromSet: trackSubset,
+                           usingDistanceFunction: distanceFunc)
   }
 }
